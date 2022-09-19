42cc6497f522e7f70f285a4eaaf313d1c238b62c
LuizKruel <112526072+LuizKruel@users.noreply.github.com>
2022-09-19 15:22:18 -0300

    Create sai.py

---
+++ sai.py
@@ -0,0 +1,13 @@
+"""
+Projeto-calculadora
+Descrição: módulo de um programa python que mostra o resultado da operações matemáticas.
+Autor: Luiz Kruel
+Data: 19/09/2022
+Versão: 0.0.5
+"""
+
+
+#Saida de dados / resultado
+
+def saida(valor):
+    print(f"\nA operação resulta em {valor}.")
