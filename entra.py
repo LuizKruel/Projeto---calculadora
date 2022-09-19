a84b4b6dc6ae711cdfc774c925a8546ab4854abd
LuizKruel <112526072+LuizKruel@users.noreply.github.com>
2022-09-19 15:22:53 -0300

    Update entra.py

--- entra.py
+++ entra.py
@@ -6,10 +6,9 @@ Data: 19/09/2022
 Versão: 0.0.5
 """
 
-#Entrada de dados
+# entrada de dados
 
 def entrada() -> list:
-    #Entra os dados de número em uma lista
     x = float(input("\nDigite o primeiro número e clique enter: "))
     y = float(input("\nDigite o segundo número e clique enter: "))
     return [x, y]
