aaecaf1026893cb2179d786035efa1dddb6166aa
LuizKruel <112526072+LuizKruel@users.noreply.github.com>
2022-09-19 15:16:01 -0300

    Create aritmetica.py

---
+++ aritmetica.py
@@ -0,0 +1,29 @@
+"""
+Projeto-calculadora
+Descrição: módulo de um programa python que realiza as 4 operações matemáticas.
+Autor: Luiz Kruel
+Data: 19/09/2022
+Versão: 0.0.5
+"""
+
+#Processamento de Dados
+
+def adicao(x: float, y: float) -> float:
+    #soma dois números.
+    return x + y
+
+
+def subtracao(x: float, y: float) -> float:
+   #subtrai um número x de um número y.
+   return x - y
+
+def multiplicacao(x: float, y: float) -> float:
+    #multiplica os dois números.
+    return x*y
+
+
+def divisao(x: float, y: float) -> float:
+    #divide x por y sendo y diferente de zero
+    if y == 0:
+        return "Não se pode dividir por zero."
+    return x/y
